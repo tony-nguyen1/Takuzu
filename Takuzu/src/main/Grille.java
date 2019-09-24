e86ef225737d7ed284f6249521cabb61f11139ff
Pierre-Antoine Chéron <pierre-antoine.cheron@etu.umontpellier.fr>
2019-09-24 21:47:19 +0200

    all done except affichage

--- Takuzu/src/main/Grille.java
+++ Takuzu/src/main/Grille.java
@@ -1,35 +1,47 @@
 package main;
 
-
+import java.util.Arrays;
 
 public class Grille {
-    private int[,] grille;
+    private int[][] grille;
     private final int HEIGHT;
     private final int WIDTH;
 
     public Grille(int width, int height) {
-        grille = new int[width, height]{-1};
+        grille = new int[width][height];
+        fill(-1);
         HEIGHT = height;
         WIDTH = width;
     }
 
     @Override
     public String toString() {
-        return "Grille{\n" + grille + '}\n';
+        String res = "";
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (grille[x][y]>=0) {
+                    res+=grille[x][y]+" ";                    
+                }else{
+                    res+="* ";
+                }
+            }
+            res+="\n";
+        }
+        return "Grille{\n" + res + "}\n";
     }
 
     public void affichage() {
         //TODO
-        throw new RuntimeException("Not implemented yet!");
+        System.out.println(toString());
     }
 
 
     public void setValue(int x, int y, int value){
-        grille[x,y] = value;
+        grille[x][y] = value;
     }
 
     public int getValue(int x, int y){
-        return grille[x,y];
+        return grille[x][y];
     }
 
     
@@ -39,5 +51,11 @@ public class Grille {
 
     public int getWIDTH() {
         return this.WIDTH;
+    }
+
+    public void fill(int value){
+        for (int[] ligne : grille) {
+            Arrays.fill(ligne, value);
+        }
     }
 }
