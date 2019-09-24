ee3d9907ff0033700ad62d9da7eba662424a10d2
Pierre-Antoine Chéron <pierre-antoine.cheron@etu.umontpellier.fr>
2019-09-24 20:32:06 +0200

    grille commit + class diagram

--- Takuzu/src/main/Grille.java
+++ Takuzu/src/main/Grille.java
@@ -1,63 +1,43 @@
 package main;
 
-import java.util.ArrayList;
+
 
 public class Grille {
-    private ArrayList<ArrayList<Integer>> grille;
-    private int longueur;
+    private int[,] grille;
+    private final int HEIGHT;
+    private final int WIDTH;
 
-    public Grille(int j) {
-        this.longueur = j;
-        grille = new ArrayList<>(longueur);
-
-        ArrayList<Integer> remplissage, ligne;
-
-        remplissage= new ArrayList<>(longueur);
-        for (int i = 0; i < longueur; i++) {remplissage.add(-1);}
-
-        for (int i = 0; i < longueur; i++) {
-            ligne = new ArrayList<>(longueur);
-            ligne.addAll(remplissage); //le nouveau array doit être remplit sinonn il est considéré vide et ne peut pas être géré correctement
-            grille.add(ligne);
-        }
+    public Grille(int width, int height) {
+        grille = new int[width, height]{-1};
+        HEIGHT = height;
+        WIDTH = width;
     }
 
     @Override
     public String toString() {
-        return "Grille{" + grille + '}';
+        return "Grille{\n" + grille + '}\n';
     }
 
     public void affichage() {
-        for (int i = 0; i < longueur; i++) { System.out.println(grille.get(i)); }
-    }
-
-    public ArrayList<ArrayList<Integer>> getGrille() {
-        return grille;
-    }
-
-    public ArrayList<Integer> getCollonne(int y)
-    {
-        ArrayList<Integer> a = new ArrayList<>(longueur);
-
-        for (int i = 0; i < longueur; i++)
-            a.add(grille.get(i).get(y));
-        return a;
+        //TODO
+        throw new RuntimeException("Not implemented yet!");
     }
 
 
-    public ArrayList<Integer> getLigne(int x)
-    {
-        ArrayList<Integer> a = new ArrayList<>(longueur);
-        a.addAll(this.grille.get(x));
-        return a;
+    public void setValue(int x, int y, int value){
+        grille[x,y] = value;
     }
 
-
-    public void ecrireValeur(int x, int y, int i)
-    {
-        /*ArrayList<Integer> unAbscisse = this.grille.get(x);
-        unAbscisse.set(y,i);*/
-        this.grille.get(y).set(x,i);
+    public int getValue(int x, int y){
+        return grille[x,y];
     }
 
+    
+    public int getHEIGHT() {
+        return this.HEIGHT;
+    }
+
+    public int getWIDTH() {
+        return this.WIDTH;
+    }
 }
--- Takuzu/src/main/Regles.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.util.Collection;
-
-public interface Regles {
-    public boolean checkTableau1D(Collection<Integer> tableau1D);
-    public boolean compareTableau1D(Collection<Integer> t1, Collection<Integer> t2);
-    public boolean estGagnant();
-}
