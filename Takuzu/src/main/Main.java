fa6161311cec2eda6be66e316f19b539b989aaf3
tony-nguyen1 <tony.nguyen@etu.umontpellier.fr>
2019-10-02 00:17:57 +0200

    Implementation d'une partie des règles.

--- Takuzu/src/main/Main.java
+++ Takuzu/src/main/Main.java
@@ -9,9 +9,10 @@ public class Main {
     public static void main(String[] args) {
        Takuzu takuzu = new Takuzu(6);
        takuzu.preRemplissage();
+
+        takuzu.checkRow(0);
+
        takuzu.affichage();
        takuzu.affichageGraphique();
-
-       takuzu.checkRow(0);
     }
 }
